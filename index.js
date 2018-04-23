b36cf9ca04e64279bf53d28a5cf6f97a03b85f88
SARAN <saranp502@hotmail.com>
2018-04-23 17:11:35 +0700

    update

--- index.js
+++ index.js
@@ -41,15 +41,11 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => {
     var drinks = [
-        { name: 'Bloody Mary', drunkness: 3 },
-        { name: 'Martini', drunkness: 5 },
-        { name: 'Scotch', drunkness: 10 }
+        { textAll: detections[0].description, email: global.email, tel: global.tel, mobile: global.mobile, name: global.name },
     ];
-    var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
 
     res.render('pages/index', {
-        drinks: drinks,
-        tagline: tagline
+        drinks: drinks
     });
 
   })
